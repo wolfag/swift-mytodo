27e9a741ad6990565a80bc8a3bddb848b897d929
wolfag <wolfag91@gmail.com>
2022-12-19 08:54:58 +0700

    Initial Commit

---
+++ MyTodo/MyTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTodoApp.swift
+//  MyTodo
+//
+//  Created by wolfag on 19/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
